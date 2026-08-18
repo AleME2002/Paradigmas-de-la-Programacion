1d7b32e9d1ff2e5c61d4358070f740113e694013
AleME2002 <martinezevansalejo@gmail.com>
2026-08-18 18:06:33 -0300

    Ejercicio 2

--- Guias/Guia1.hs
+++ Guias/Guia1.hs
@@ -1,6 +1,6 @@
 --------------------Ejercicio 1--------------------
 
--- max2 :: (Float -> Float) -> Float
+-- max2 :: (Float -> Float) -> Float                     max2 :: (Float -> Float) -> Float
 -- normaVectorial :: (Float, Float) -> Float
 -- subtract :: Float -> Float -> Float
 -- predecesor :: Float -> Float
@@ -8,5 +8,22 @@
 -- dosVeces :: (a -> a) -> a -> a
 -- flipAll :: 
 -- flipRaro :: 
+
+
+--------------------Ejercicio 2--------------------
+
+curry :: ((a, b) -> c) -> a -> b -> c
+curry f x y = f (x, y)
+
+uncurry :: (a -> b -> c) -> (a, b) -> c
+uncurry f (x, y) = f x y
+
+-- curryN no se podria ya que necesitaria una con 4 uplas y mas
+
+
+--------------------Ejercicio 3--------------------
+
+
+
 
 
