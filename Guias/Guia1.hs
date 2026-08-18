072341a92b28e1f8bd7e6911c053ee1d08926d61
AleME2002 <martinezevansalejo@gmail.com>
2026-08-18 18:20:09 -0300

    Ejercicio 3

--- Guias/Guia1.hs
+++ Guias/Guia1.hs
@@ -23,6 +23,30 @@ uncurry f (x, y) = f x y
 
 --------------------Ejercicio 3--------------------
 
+listaDesde :: Num t => t -> [t]
+listaDesde x = x : listaDesde (x+1)
+
+esMultiploDe10 :: Integral a => a -> Bool
+esMultiploDe10 n = mod n 10 == 0
+
+takeHastaMultiploDe10 :: Integral a => [a] -> [a]
+takeHastaMultiploDe10 [] = []
+takeHastaMultiploDe10 (x:xs) = if esMultiploDe10 x then [] else x : takeHastaMultiploDe10 xs
+
+{-Mostrar paso a paso cómo reduce Haskell la expresión takeHastaMultiploDe10 (listaDesde 29)
+takeHastaMultiploDe10 (listaDesde 29)
+takeHastaMultiploDe10 (29 : listaDesde (29 + 1))
+if esMultiploDe10 29 then [] else 29 : takeHastaMultiploDe10 (listaDesde (29 + 1))
+29 : takeHastaMultiploDe10 (listaDesde 30)
+29 : takeHastaMultiploDe10 (30 : listaDesde (30 + 1))
+29 : (if esMultiploDe10 30 then [] else 30 : takeHastaMultiploDe10 (listaDesde (30 + 1)))
+29 : [] 
+[29]
+-}
+
+
+--------------------Ejercicio 4--------------------
+
 
 
 
