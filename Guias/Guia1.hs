aa2388436f0fc1c2cd44335c8c44f1cdaf0c57e2
AleME2002 <martinezevansalejo@gmail.com>
2026-08-18 17:53:38 -0300

    Ejercicio 1

---
+++ Guias/Guia1.hs
@@ -0,0 +1,12 @@
+--------------------Ejercicio 1--------------------
+
+-- max2 :: (Float -> Float) -> Float
+-- normaVectorial :: (Float, Float) -> Float
+-- subtract :: Float -> Float -> Float
+-- predecesor :: Float -> Float
+-- evaluarEnCero :: (Float -> a) -> a
+-- dosVeces :: (a -> a) -> a -> a
+-- flipAll :: 
+-- flipRaro :: 
+
+
